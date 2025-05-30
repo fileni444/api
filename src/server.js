a1c2894b48c0f48b3806f1c4ac2cdb253445e48c
fileni444 <gustavo.fileni@sou.unifeob.edu.br>
2025-05-30 16:59:05 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,6 +4,6 @@ require('./config/db'); // inicializa o banco
 const api = require('./api'); // app configurado já com as rotas e middlewares
 
 // Sobe o servidor
-app.listen(4040, '0.0.0.0', () => {
+api.listen(4040, '0.0.0.0', () => {
   console.log('\nAPI INICIALIZADA!!');
 });
